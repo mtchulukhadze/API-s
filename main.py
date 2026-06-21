f19ce7ac1f1e569577442868d4b8bef243075ad6
mtchulukhadze <mikheili.tchulukhadze.1@btu.edu.ge>
2026-06-21 20:05:23 +0400

    api's

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {
-        "data": {
-            "name": "mikheil"
-        }
-    }
+db_path = "database.db"
 
-@app.get("/about")
-def about():
-    return {
-        "about": {
-            "info": "page"
-        }
-    }
+def db():
+    conn  = sqlite3.connect(db_path)
+    conn.execute("""
+        create table if not exists logs2 (
+        id int,
+        param_name varchar(32),
+        param_value int
+    )
+    """)
+    conn.commit()
+    conn.close()
 
-@app.get("/home")
-def home():
-    return {
-        "home": {
-            "name": "home"
-        }
-    }
+
+def insert(param_name, param_value):
+    conn =sqlite3.connect(db_path)
+    conn.execute(
+        """insert into logs2 ( param_name, param_value ) values (?, ?)""",
+        ( param_name, param_value )
+    )
+    conn.commit()
+    conn.close()
+
+db()
+
+
+@app.get("/set_parameter")
+def set_param(param_name, param_value):
+    insert(param_name, param_value)
+
+    return {"status": "ok", "name": param_name, "value": param_value}
+
+@app.get("/logs")
+def get_logs():
+
+    conn = sqlite3.connect(db_path)
+    rows = conn.execute(
+        "select param_name, param_value from logs2"
+    ).fetchall()
+    #conn.execute()
+    conn.close()
+
+    return [
+        {"param_name": r[0], "param_value": r[1]}
+        for r in rows
+    ]
+
